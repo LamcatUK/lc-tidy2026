9679ee3c05870bac1bf1fa3f8f937f8b134e749f
LamcatUK <git@lamcat.co.uk>
2026-02-22 19:03:34 +0000

    email

--- blocks/lc-cta.php
+++ blocks/lc-cta.php
@@ -21,8 +21,9 @@ defined( 'ABSPATH' ) || exit;
 				</div>
 			</div>
 			<div class="col-md-4 d-flex flex-column align-items-center gap-3">
-				<a class="button button--lg w-100 text-center" href="tel:<?= parse_phone( get_field( 'contact_phone' ) ); ?>"><i class="fa-solid fa-phone me-2"></i> Call Now</a>
-				<a class="d-sm-none button button--lg w-100 has-whatsapp-background-color text-center" href="tel:<?= parse_phone( get_field( 'contact_phone' ) ); ?>"><i class="fa-brands fa-whatsapp me-2"></i> WhatsApp Us</a>
+				<a class="button button--lg w-100 text-center" href="tel:<?= parse_phone( get_field( 'contact_phone', 'option' ) ); ?>"><i class="fa-solid fa-phone me-2"></i> Call Now</a>
+				<a class="d-sm-none button button--lg w-100 has-whatsapp-background-color text-center" href="tel:<?= parse_phone( get_field( 'contact_phone', 'option' ) ); ?>"><i class="fa-brands fa-whatsapp me-2"></i> WhatsApp Us</a>
+				<a class="button button--lg w-100 text-center" href="mailto:<?= antispambot( get_field( 'contact_email', 'option' ) ); ?>"><i class="fa-solid fa-envelope me-2"></i> Email Us</a>
 				<a class="button button--lg button--outline button--outline-white w-100 text-center" href="/contact/">Get a Free Quote</a>
 			</div>
 		</div>
--- blocks/lc-hero.php
+++ blocks/lc-hero.php
@@ -41,8 +41,9 @@ defined( 'ABSPATH' ) || exit;
 				if ( ! $contact_page || get_the_ID() !== $contact_page->ID ) {
 					?>
 			<div class="col-12 py-4 d-flex flex-wrap justify-content-center gap-4">
-				<a class="button button--lg" href="tel:<?= parse_phone( get_field( 'contact_phone' ) ); ?>"><i class="fa-solid fa-phone me-2"></i> Call Now</a>
-				<a class="d-sm-none button button--lg has-whatsapp-background-color" href="tel:<?= parse_phone( get_field( 'contact_phone' ) ); ?>"><i class="fa-brands fa-whatsapp me-2"></i> WhatsApp Us</a>
+				<a class="button button--lg" href="tel:<?= parse_phone( get_field( 'contact_phone', 'option' ) ); ?>"><i class="fa-solid fa-phone me-2"></i> Call Now</a>
+				<a class="d-sm-none button button--lg has-whatsapp-background-color" href="tel:<?= parse_phone( get_field( 'contact_phone', 'option' ) ); ?>"><i class="fa-brands fa-whatsapp me-2"></i> WhatsApp Us</a>
+				<a class="button button--lg" href="mailto:<?= antispambot( get_field( 'contact_email', 'option' ) ); ?>"><i class="fa-solid fa-envelope me-2"></i> Email Us</a>
 				<a class="button button--lg button--outline" href="/contact/">Get a Free Quote</a>
 			</div>
 					<?php
