bcf12d3d75a57ad709d7c846f922861772f087a5
LamcatUK <git@lamcat.co.uk>
2026-02-28 18:01:57 +0000

    who grid

---
+++ blocks/lc-who-grid.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Block template for LC Who Grid.
+ *
+ * @package lc-tidy2026
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+?>
+<section class="who-grid">
+	<div class="container">
+		<h2><?= esc_html( get_field( 'title' ) ); ?></h2>
+		<p class="has-600-font-size mb-4"><?= esc_html( get_field( 'intro' ) ); ?></p>
+		<div class="who-grid__grid row g-4 mb-4">
+			<?php
+			while ( have_rows( 'items' ) ) {
+				the_row();
+				$l = get_sub_field( 'item' );
+				?>
+			<div class="col-md-6 col-lg-3 who-grid__item">
+				<div class="who-grid__icon-wrapper">
+					<img src="<?= esc_url( get_sub_field( 'icon' )['url'] ); ?>" alt="<?= esc_attr( get_sub_field( 'icon' )['alt'] ); ?>" class="who-grid__icon">
+				</div>
+				<h3 class="who-grid__item-title has-600-font-size mb-0"><?= esc_html( get_sub_field( 'title' ) ); ?></h3>
+				<p class="who-grid__item-description"><?= esc_html( get_sub_field( 'description' ) ); ?></p>
+			</div>
+				<?php
+			}
+			?>
+		</div>
+		<p class="has-600-font-size"><?= esc_html( get_field( 'outro' ) ); ?></p>
+	</div>
+</section>
--- inc/lc-blocks.php
+++ inc/lc-blocks.php
@@ -19,6 +19,23 @@ function acf_blocks() {
 
 		acf_register_block_type(
 			array(
+				'name'            => 'lc_who_grid',
+				'title'           => __( 'LC Who Grid' ),
+				'category'        => 'layout',
+				'icon'            => 'cover-image',
+				'render_template' => 'blocks/lc-who-grid.php',
+				'mode'            => 'edit',
+				'supports'        => array(
+					'mode'      => false,
+					'anchor'    => true,
+					'className' => true,
+					'align'     => true,
+				),
+			)
+		);
+
+		acf_register_block_type(
+			array(
 				'name'            => 'lc_service_list',
 				'title'           => __( 'LC Service List' ),
 				'category'        => 'layout',
