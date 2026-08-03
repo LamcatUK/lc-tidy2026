37f059bdaea5144c1b490d9079b5c64b7f69e313
LamcatUK <git@lamcat.co.uk>
2026-08-03 19:24:36 +0100

    cta call

--- blocks/lc-cta.php
+++ blocks/lc-cta.php
@@ -20,7 +20,8 @@ defined( 'ABSPATH' ) || exit;
 				</div>
 			</div>
 			<div class="col-md-4 d-flex flex-column align-items-center gap-3">
-				<a class="button button--lg w-100 text-center" href="tel:<?= parse_phone( get_field( 'contact_phone', 'option' ) ); ?>"><i class="fa-solid fa-phone me-2"></i> Call Now</a>
+				<a class="button button--lg w-100 text-center d-none d-sm-inline-block" href="tel:<?= parse_phone( get_field( 'contact_phone', 'option' ) ); ?>"><i class="fa-solid fa-phone me-2"></i> Call <?= get_field( 'contact_phone', 'option' ); ?></a>
+				<a class="button button--lg w-100 text-center d-sm-none" href="tel:<?= parse_phone( get_field( 'contact_phone', 'option' ) ); ?>"><i class="fa-solid fa-phone me-2"></i> Call Now</a>
 				<?= do_shortcode( '[whatsapp_link class="d-sm-none button button--lg has-whatsapp-background-color" icon=true text="WhatsApp Us"]'); ?>
 				<!-- <a class="button button--lg w-100 text-center" href="mailto:<?= antispambot( get_field( 'contact_email', 'option' ) ); ?>"><i class="fa-solid fa-envelope me-2"></i> Email Us</a> -->
 				<a class="button button--lg button--outline button--outline-white w-100 text-center" href="/contact/">Get a Free Quote</a>
